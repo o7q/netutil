81d320fbfa88d49cd0af6117c2dfe352c52317f4
o7q <jev.o7q@gmail.com>
2022-08-31 13:47:17 -0500

    Enable g++ static linking

--- src/netutil/compile.bat
+++ src/netutil/compile.bat
@@ -1,5 +1,5 @@
 @echo off
 color e
-g++ *.cpp -o "..\..\..\PackagedBuild\netutil\netutil.exe"
+g++ *.cpp -o "..\..\..\PackagedBuild\netutil\netutil.exe" -static-libstdc++ -static-libgcc
 echo.
 pause
